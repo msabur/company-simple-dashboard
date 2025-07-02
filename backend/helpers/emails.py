7bd6a8df87605757dfca15fead668f275659d149
msabur <58122827+msabur@users.noreply.github.com>
2025-07-02 16:21:40 -0400

    restructuring - helper folder

--- backend/helpers/emails.py
+++ backend/helpers/emails.py
@@ -1,15 +1,15 @@
 from fastapi_mail import FastMail, MessageSchema, MessageType
 from config import MAIL_CONFIG
-from schemas import EmailSchema
+from schemas import EmailDetails
 
-async def send_signup_verification_email(email: EmailSchema):
+async def send_signup_verification_email(email_details: EmailDetails):
     message = MessageSchema(
         subject='Your Alora verification code',
-        recipients=email.recipients,
-        template_body=email.body,
+        recipients=email_details.recipients,
+        template_body=email_details.body,
         subtype=MessageType.html
     )
 
     fm = FastMail(MAIL_CONFIG)
     await fm.send_message(message, template_name="signup_verification.jinja2")
-    print(f"Email sent to {email.recipients}!")
+    print(f"Email sent to {email_details.recipients}!")
